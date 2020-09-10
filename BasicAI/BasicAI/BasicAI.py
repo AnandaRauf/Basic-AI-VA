3d74d838dfe4acb8c088f91d91165656fc9bf141
Ananda Rauf <uzumaki.nanda@rocketmail.com>
2020-09-10 13:55:36 +0700

    Basic AI VA Version 1.0 Dev by Ananda Rauf Maududi

---
+++ BasicAI/BasicAI/BasicAI.py
@@ -0,0 +1,36 @@
+import pyttsx3
+
+mesin = pyttsx3.init()
+
+
+def menu():
+    print("Menu Basic AI\n")
+    print
+    print("1.VoiceTxt")
+    print("2.Voice")
+
+def VoiceTxt():
+    file = open("TestAI.txt")
+    bacafile = file.readlines()
+    bacabaris = bacafile[0]
+    bicara = mesin.say(bacabaris)
+    bicara = mesin.runAndWait()
+    print(bicara)
+
+def Voice():
+    berbicara = mesin.say("Test")
+    berbicara = mesin.runAndWait()
+    print(berbicara)
+
+menu()
+pilihmenu = int(input("Pilih menu Basic AI:"))
+if pilihmenu == 1:
+    VoiceTxt()
+elif pilihmenu == 2:
+    Voice()
+
+
+
+
+
+
